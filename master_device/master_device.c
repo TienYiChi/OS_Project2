2df6c5c5efcf354c210e9a25e3fc6d38ed3ee8b6
r07922097 <r07922097@ntu.edu.tw>
2020-06-23 15:33:31 +0800

    address offset was incorrect

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -215,7 +215,8 @@ int master_open(struct inode *inode, struct file *filp)
 static long master_ioctl(struct file *filp, unsigned int ioctl_num, unsigned long ioctl_param)
 {
 	long ret = -EINVAL;
-	size_t len = 0, data_size = 0, offset = 0, file_size = 0;
+	size_t len = 0, data_size = 0, file_size = 0;
+	unsigned long offset = 0;
 	char *tmp;
 	void *buf_addr = NULL;
 	pgd_t *pgd;
@@ -254,7 +255,7 @@ static long master_ioctl(struct file *filp, unsigned int ioctl_num, unsigned lon
       			len = ksend(sockfd_cli, buf_addr, len, 0);
 				offset += 1;
 				data_size += len;
-				buf_addr = buf_addr + PAGE_SIZE*sizeof(char);
+				buf_addr = buf_addr + offset*PAGE_SIZE;
     		}
     		ret = data_size;
 			break;
